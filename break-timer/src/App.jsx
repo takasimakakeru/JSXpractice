7a87833fcdeeca39bb6a6e22875644f3412de3d0
takasimakakeru <kyunpangoogle@outlook.jp>
2026-07-07 00:27:56 +0000

    a

---
+++ break-timer/src/App.jsx
@@ -0,0 +1,6 @@
+import { useState, useEffect } from "react";
+function App() {
+	const [alarmList, setAlarmList] = useState(["09:30"," 10:30", "11:30", "13:30", "14:30", "15:30"]);
+	return (
+	);
+}
