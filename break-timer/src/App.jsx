8ea827ac86f4e3b50dde4a4eda097d5f409b9016
takasimakakeru <kyunpangoogle@outlook.jp>
2026-07-07 03:22:00 +0000

    a

--- break-timer/src/App.jsx
+++ break-timer/src/App.jsx
@@ -1,6 +1,25 @@
-import { useState, useEffect } from "react";
 function App() {
-	const [alarmList, setAlarmList] = useState(["09:30"," 10:30", "11:30", "13:30", "14:30", "15:30"]);
-	return (
-	);
+  // 1. 変数（State）を準備する場所
+  const [alarmList, setAlarmList] = useState(["09:30:00","10:30:00", "11:30:00", "13:30:00", "14:30:00", "15:30:00"]);
+  const [currentTime, setCurrentTime] = useState(""); // ★これ！
+
+  // 2. 裏で動く処理（Effect）の場所
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const now = new Date();
+      const timeString = now.toLocaleTimeString(); 
+
+      setCurrentTime(timeString); // ★コメントを外して本物にする！
+
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  // 3. 画面の見た目（JSX）の場所
+  return (
+    <div>
+      {/* ★中括弧で変数を表示する！ */}
+      <h2>今の時間：{currentTime}</h2>
+    </div>
+  );
 }
