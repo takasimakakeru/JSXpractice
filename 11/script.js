54bc24a75c493d9a416d84ff39efa686859ce959
takasimakakeru <kyunpangoogle@outlook.jp>
2026-07-14 02:52:40 +0000

    a

--- 11/script.js
+++ 11/script.js
@@ -1,9 +1,11 @@
+'use strict';
+
 const list = [
-  '01.jpg',
-  '02.jpg',
-  '03.jpg',
-  '04.jpg',
-  '05.jpg',
+  'images/01.jpg',
+  'images/02.jpg',
+  'images/03.jpg',
+  'images/04.jpg',
+  'images/05.jpg',
   'images/06.jpg',
   'images/07.jpg',
   'images/08.jpg',
