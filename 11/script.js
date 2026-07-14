528a9ad10a89939ca92999bae1ba0df2bc100b87
takasimakakeru <kyunpangoogle@outlook.jp>
2026-07-14 01:50:27 +0000

    a

---
+++ 11/script.js
@@ -0,0 +1,12 @@
+const list = [
+  '01.jpg',
+  '02.jpg',
+  '03.jpg',
+  '04.jpg',
+  '05.jpg',
+  'images/06.jpg',
+  'images/07.jpg',
+  'images/08.jpg',
+  'images/09.jpg',
+  'images/10.jpg',
+];
